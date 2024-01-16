f978d2b59c06457e70010ecd41a41a21e4c44699
FoxLlik <yukionna697@gmail.com>
2024-01-16 10:30:42 +0800

    Төгсөлтийн ажил засварууд!

--- frontend/src/views/Student/Graduation/PrintMongolia/index.jsx
+++ frontend/src/views/Student/Graduation/PrintMongolia/index.jsx
@@ -82,13 +82,6 @@ export default function PrintMongolia()
     }
 
     return (
-
-        // энэ мөрний стайлын учир сайн олдохгүй л байна.
-        // Харагдах байдал нь print preview дээр зүгээр
-        // хэрнээ хэвлэхээр арагшаа гүйгээд байна.
-
-        // Ерөнхийдөө ажиллагаа одоогоор хэвийн. Өөр төхөөрөмжүүд дээр тестлэж, засвар хийж цэгцлэх шаардлагатай
-
         <div
             className={`vh-100 position-relative d-flex flex-column justify-content-end align-items-center bg-white`}
             style={{ fontFamily: 'Arial', color: 'black' }}
@@ -99,23 +92,25 @@ export default function PrintMongolia()
             {/* Үндсэн хэсэг */}
             <div className='text-center' style={{ width: '100%', lineHeight: '30px', fontSize: '19px' }} >
                 <div className='m-auto' style={{ width: '700px' }}>
-                    {data?.student?.citizenship?.name} улсын иргэн {data?.student?.lastname} {data?.student?.first_name} нь {data?.student?.group?.join_year?.substring(0, 4)}-{data?.lesson_year?.substring(5, 9)} онд {data?.student?.school_name}д
+                    {data?.student?.citizenship?.name} улсын иргэн <span className='text-uppercase fw-bolder'>{data?.student?.lastname} {data?.student?.first_name}</span> нь
+                    <br />
+                    {data?.student?.group?.join_year?.substring(0, 4)}-{data?.lesson_year?.substring(5, 9)} онд {data?.student?.school_name}д
                     "{data?.student?.group?.profession?.name}" мэргэжил ({data?.student?.group?.degree?.degree_code}{data?.student?.group?.profession?.code})-ээр суралцаж төгссөн тул Их сургуулийн захирлын
                     {data?.decision_date?.substring(0, 4)} оны {data?.decision_date?.substring(5, 7)} {dugeerUg(data?.decision_date?.substring(5, 7) && data?.decision_date?.substring(5, 7).charAt(data?.decision_date?.substring(5, 7).length - 1))} сарын {data?.decision_date?.substring(8, 10)} өдрийн {data?.graduation_number} {dugeerUg(data?.graduation_number && data?.graduation_number.charAt(data?.graduation_number.length - 1))} тушаалаар
-                    <span className='text-uppercase'> {data?.student?.group?.profession?.dep_name} {data?.student?.group?.degree?.degree_name}</span>-ын зэрэг олгов.
+                    <span style={{ whiteSpace: 'nowrap' }}><span className='text-uppercase'> {data?.student?.group?.profession?.dep_name} {data?.student?.group?.degree?.degree_name}</span>-ын зэрэг олгов.</span>
                 </div>
             </div>
 
             {/* Гарын үсгийн хэсэг */}
             <div style={{ bottom: '0', fontSize: '15px' }} >
-                <div style={{ paddingLeft: '70px', paddingRight: '70px', lineHeight: '18px', marginTop: '100px', marginBottom: '10px' }} >
+                <div style={{ paddingLeft: '100px', paddingRight: '100px', lineHeight: '18px', marginTop: '100px', marginBottom: '10px' }} >
                     <div className='d-flex w-100 text-center justify-content-center'>
 
                         {
                             listArr.map((val, idx) =>
                             {
                                 return (
-                                    <div className='w-25 d-flex flex-column pt-2 pb-2' style={{ paddingRight: '7px', paddingLeft: '7px' }} key={idx} >
+                                    <div className='d-flex flex-column pt-2 pb-2' style={{ paddingRight: '20px', paddingLeft: '20px' }} key={idx} >
                                         <span className='border-top-black'>{val?.position_name}</span>
                                         <span>{val?.last_name}{val?.first_name}</span>
                                     </div>
