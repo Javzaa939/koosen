e564bb588c9ea6b7fb9b321390bea94595d81a70
FoxLlik <yukionna697@gmail.com>
2024-01-17 20:43:27 +0800

    national

--- frontend/src/views/Student/Graduation/PrintNational/index.jsx
+++ frontend/src/views/Student/Graduation/PrintNational/index.jsx
@@ -33,10 +33,10 @@ export default function PrintMongolia()
         {
             getAllData()
 
-            window.onafterprint = function()
-            {
-                window.close()
-            }
+            // window.onafterprint = function()
+            // {
+            //     window.close()
+            // }
         },
         []
     )
@@ -54,32 +54,32 @@ export default function PrintMongolia()
 
     function tooBichih(too)
     {
-        return ( <span style={{ fontFamily: 'CMSUB', fontSize: '30px' }}>{too}</span> )
+        return ( <span style={{ fontFamily: 'CMSUB', fontSize: '25px' }}>{too}</span> )
     }
 
     function tushaal(text)
     {
         let slash = text.split('/')
 
-        return <><span>ᠠ/</span><span style={{ fontFamily: 'CMSUB', fontSize: '30px' }}>{slash[1]}</span></>
+        return <><span>ᠠ/</span><span style={{ fontFamily: 'CMSUB', fontSize: '25px' }}>{slash[1]}</span></>
     }
 
     return (
-        <div className='vh-100 position-relative' style={{ fontFamily: 'mongolianScript', fontSize: '24px', padding: '125px 160px 100px 240px' }}>
+        <div className='vh-100 position-relative' style={{ fontFamily: 'mongolianScript', fontSize: '16px', padding: '125px 160px 100px 260px' }}>
             {isLoading && Loader}
 
             <div className='d-flex justify-content-between h-100' >
                 <div className='d-flex'>
                     <div className='text-center' style={{ writingMode: 'vertical-lr' }} >
-                        ᠳ᠋ᠤᠭᠠᠷ <span style={{ fontFamily: 'cmdashitseden' }}>{data?.student?.group?.degree?.degree_code}</span><span style={{ fontFamily: 'CMSUB', fontSize: '30px' }}>{data?.diplom_num}</span>
+                        ᠳ᠋ᠤᠭᠠᠷ <span style={{ fontFamily: 'cmdashitseden', fontSize: '24px' }}>{data?.student?.group?.degree?.degree_code}</span>{tooBichih(data?.diplom_num)}
                     </div>
-                    <div style={{ writingMode: 'vertical-lr', marginLeft: '30px' }} >
-                        ᠮᠣᠩᠭᠣᠯ ᠤᠯᠤᠰ ᠤ᠋ᠨ ᠢᠷᢉᠡᠨ {data?.student?.last_name_uig} ᠶᠢᠨ / ᠤᠨ {data?.student?.first_name_uig} ᠨᠢ <span style={{ fontFamily: 'CMSUB', fontSize: '30px' }}>{data?.student?.group?.join_year?.substring(0, 4)} - {data?.lesson_year?.substring(5, 9)}</span> ᠣᠨ ᠳ᠋ᠤ {data?.student?.school_name_uig} ᠳ᠋ᠤ {data?.student?.group?.profession?.name_uig} ᠮᠡᠷᢉᠡᠵᠢᠯ <span style={{ fontFamily: 'cmdashitseden' }}>({data?.student?.group?.degree?.degree_code}</span><span style={{ fontFamily: 'CMSUB', fontSize: '30px' }}>{data?.diplom_num}</span>)
+                    <div style={{ writingMode: 'vertical-lr', marginLeft: '36px', lineHeight: '46px' }} >
+                        ᠮᠣᠩᠭᠣᠯ ᠤᠯᠤᠰ ᠤ᠋ᠨ ᠢᠷᢉᠡᠨ {data?.student?.last_name_uig} ᠶᠢᠨ / ᠤᠨ {data?.student?.first_name_uig} ᠨᠢ <span style={{ fontFamily: 'CMSUB', fontSize: '25px' }}>{data?.student?.group?.join_year?.substring(0, 4)} - {data?.lesson_year?.substring(5, 9)}</span> ᠣᠨ ᠳ᠋ᠤ {data?.student?.school_name_uig} ᠳ᠋ᠤ {data?.student?.group?.profession?.name_uig} ᠮᠡᠷᢉᠡᠵᠢᠯ <span style={{ fontFamily: 'cmdashitseden', fontSize: '24px' }}>({data?.student?.group?.degree?.degree_code}</span>{tooBichih(data?.diplom_num)})
                         ᠢ᠋ᠶᠡᠷ ᠰᠤᠷᠤᠯᠴᠠᠵᠤ ᠲᠡᢉᠦᠰᠦᢉᠰᠡᠨ ᠲᠤᠯᠠ ᠶᠡᢈᠡ ᠰᠤᠷᠭᠠᠭᠤᠯᠢ ᠶ᠋ᠢᠨ ᠵᠠᢈᠢᠷᠤᠯ ᠤ᠋ᠨ {tooBichih(data?.decision_date?.substring(0, 4))} ᠣᠨ ᠤ᠋ {tooBichih(data?.decision_date?.substring(5, 7))} ᠳ᠋ᠤᠭᠠᠷ ᠰᠠᠷ᠎ᠠ ᠶ᠋ᠢᠨ {tooBichih(data?.decision_date?.substring(8, 10))}{tooBichih('-')} ᠪ
                         ᠡᠳᠦᠷ ᠦ᠋ᠨ {tushaal(data?.graduation_number)} ᠳ᠋ᠤᠭᠠᠷ ᠲᠤᠰᠢᠶᠠᠯ ᠢ᠋ᠶᠠᠷ {data?.student?.group?.profession?.dep_name_uig} ᠶᠢᠨ {data?.student?.group?.degree?.degree_uig_name} ᠤ᠋ᠨ ᠵᠡᠷᢉᠡ ᠣᠯᠭᠤᠪᠠ.
                     </div>
                 </div>
-                <div className='d-flex' style={{ fontSize: '18px', lineHeight: '35px' }}>
+                <div className='d-flex' style={{ fontSize: '14px', lineHeight: '42px' }}>
                 {
                     listArr.map((val, idx) =>
                     {
@@ -93,7 +93,7 @@ export default function PrintMongolia()
                 }
                     <div className='d-flex justify-content-between' style={{ writingMode: 'vertical-rl', paddingTop: '30px', paddingBottom: '30px' }} >
                         <span>ᠤᠯᠠᠭᠠᠨᠪᠠᠭᠠᠲᠤᠷ ᠬᠣᠲᠠ</span>
-                        <span>ᠪᠦᠷᠢᠳᢈᠡᠯ ᠦ᠋ᠨ ᠳ᠋ᠤᠭᠠᠷ <span style={{ fontFamily: 'CMSUB', fontSize: '28px' }}>{data?.registration_num}</span></span>
+                        <span>ᠪᠦᠷᠢᠳᢈᠡᠯ ᠦ᠋ᠨ ᠳ᠋ᠤᠭᠠᠷ <span style={{ fontFamily: 'CMSUB', fontSize: '20px' }}>{data?.registration_num}</span></span>
                     </div>
                 </div>
             </div>
