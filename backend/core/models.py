d847b4ac6ef8b6664d39287f6e46ebfa543ab090
Daria1123 <dariadaria0709@gmail.com>
2024-01-25 14:05:21 +0800

    check

--- backend/core/models.py
+++ backend/core/models.py
@@ -144,9 +144,9 @@ class SubOrgs(models.Model):
     tsol_name_eng = models.CharField(max_length=500, null=True, blank=True, verbose_name="Цол нэр англи")
     tsol_name_uig = models.CharField(max_length=500, null=True, blank=True, verbose_name="Цол нэр уйгаржин")
 
-    # erdem_tsol_name = models.CharField(max_length=250, verbose_name='Эрдмийн цол нэр', null=True, blank=True)
-    # erdem_tsol_name_eng = models.CharField(max_length=500, null=True, blank=True, verbose_name="Эрдмийн цол нэр англи")
-    # erdem_tsol_name_uig = models.CharField(max_length=500, null=True, blank=True, verbose_name="Эрдмийн цол нэр уйгаржин")
+    erdem_tsol_name = models.CharField(max_length=250, verbose_name='Эрдмийн цол нэр', null=True, blank=True)
+    erdem_tsol_name_eng = models.CharField(max_length=500, null=True, blank=True, verbose_name="Эрдмийн цол нэр англи")
+    erdem_tsol_name_uig = models.CharField(max_length=500, null=True, blank=True, verbose_name="Эрдмийн цол нэр уйгаржин")
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- frontend/src/views/Student/Attachment/PrintMongolia/index.jsx
+++ frontend/src/views/Student/Attachment/PrintMongolia/index.jsx
@@ -56,8 +56,6 @@ export default function PrintAttachmentMongolia()
         })
     ]);
 
-    console.log(datas)
-
     useEffect(
         () =>
         {
