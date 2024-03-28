e3aca47ae2c6831e41662762170d1d52ad095abc
gracexnn <102289404+gracexnn@users.noreply.github.com>
2024-03-28 19:21:01 +0800

    modal update

--- frontend/src/views/Elselt/User/StateModal/index.jsx
+++ frontend/src/views/Elselt/User/StateModal/index.jsx
@@ -1,6 +1,6 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
-import { Button, Form, FormFeedback, Input, Label, Modal, ModalBody, ModalHeader } from 'reactstrap';
+import { Button, Form, FormFeedback, Input, Label, Modal, ModalBody, ModalHeader, Popover, PopoverBody, PopoverHeader, UncontrolledPopover, UncontrolledTooltip } from 'reactstrap';
 import { validate } from '@utils';
 import useApi from '@hooks/useApi';
 import useLoader from '@hooks/useLoader';
@@ -11,6 +11,8 @@ function StateModal({ stateModalHandler, stateModal, selectedStudents, stateop, 
 
     const { formState: { errors }, handleSubmit, control, reset } = useForm(validate(validateSchema));
     const { Loader, isLoading, fetchData } = useLoader({isFullScreen: true});
+
+    const [displayPopover, setDisplayPopover] = useState(false)
 
     const admissionStateChangeApi = useApi().elselt.admissionuserdata.all;
 
@@ -26,6 +28,14 @@ function StateModal({ stateModalHandler, stateModal, selectedStudents, stateop, 
         };
     };
 
+    function popoverHandler() {
+        setDisplayPopover(!displayPopover)
+    }
+
+    const state1 = selectedStudents.filter(data => data?.state === 1).length
+    const state2 = selectedStudents.filter(data => data?.state === 2).length
+    const state3 = selectedStudents.filter(data => data?.state === 3).length
+
     return (
         <Modal centered toggle={stateModalHandler} isOpen={stateModal}>
             <ModalHeader toggle={stateModalHandler}>
@@ -33,43 +43,87 @@ function StateModal({ stateModalHandler, stateModal, selectedStudents, stateop, 
             </ModalHeader>
             <ModalBody>
                 <Form className='' onSubmit={handleSubmit(onSubmit)}>
-                    <div>
-                    {
-                        stateop.map((data, idx) => {
-                            return(
-                                <Fragment key={idx}>
-                                    <div className='d-flex align-items-center'>
-                                        <Controller
-                                            control={control}
-                                            defaultValue=''
-                                            name="state"
-                                            render={({ field: { value, onChange } }) => {
-                                                return(
-                                                    <Input
-                                                        type='radio'
-                                                        name='state'
-                                                        id={`radio${idx}`}
-                                                        value={data?.id}
-                                                        checked={value === data?.id}
-                                                        onChange={(e) => {
-                                                            onChange(Number(data?.id) || '')
-                                                        }}
-                                                        className='m-50 p-50'
-                                                        invalid={errors?.state}
-                                                    >
-                                                    </Input>
-                                                )
-                                            }}
-                                        />
-                                        <Label for={`radio${idx}`} className='mt-25'>
-                                            {data?.name}
-                                        </Label>
+                    <div className='d-flex justify-content-between'>
+                        <div>
+                        {
+                            stateop.map((data, idx) => {
+                                return(
+                                    <Fragment key={idx}>
+                                        <div className='d-flex align-items-center'>
+                                            <Controller
+                                                control={control}
+                                                defaultValue=''
+                                                name="state"
+                                                render={({ field: { value, onChange } }) => {
+                                                    return(
+                                                        <Input
+                                                            type='radio'
+                                                            name='state'
+                                                            id={`radio${idx}`}
+                                                            value={data?.id}
+                                                            checked={value === data?.id}
+                                                            onChange={(e) => {
+                                                                onChange(Number(data?.id) || '')
+                                                            }}
+                                                            className='m-50 p-50'
+                                                            invalid={errors?.state}
+                                                        >
+                                                        </Input>
+                                                    )
+                                                }}
+                                            />
+                                            <Label for={`radio${idx}`} className='mt-25'>
+                                                {data?.name}
+                                            </Label>
+                                        </div>
+                                    </Fragment>
+                                )
+                            })
+                        }
+                        {errors?.state && <FormFeedback className='d-block'>{errors?.state?.message}</FormFeedback>}
+                        </div>
+                        <div>
+                            {
+                                selectedStudents.length > 0 &&
+                                <>
+                                    <div
+                                        className='bg-light-info p-50 rounded-3'
+                                        id='students_tooltip'
+                                        onMouseEnter={() => setDisplayPopover(true)}
+                                        onMouseLeave={() => setDisplayPopover(false)}
+                                        onClick={() => popoverHandler()}
+                                        style={{ cursor:'help' }}
+                                    >
+                                        Сонгогдсон оюутны тоо: <b>{selectedStudents.length}</b>
                                     </div>
-                                </Fragment>
-                            )
-                        })
-                    }
-                    {errors?.state && <FormFeedback className='d-block'>{errors?.state?.message}</FormFeedback>}
+                                    <Popover
+                                        // isOpen={true}
+                                        isOpen={displayPopover}
+                                        placement='auto'
+                                        target={"students_tooltip" || ""}
+                                        toggle={popoverHandler}
+                                    >
+                                        <PopoverHeader>
+                                            Сонгогдсон оюутнуудын нэрс
+                                        </PopoverHeader>
+                                        <PopoverBody
+                                            className='overflow-hidden'
+                                            style={{ maxHeight: 230 }}
+                                        >
+                                            <div className='m-25 px-1 p-25' style={{ borderLeft: `8px solid ${'#4287f5'}`, borderTop: '5px solid transparent', borderBottom: '5px solid transparent' }}>
+                                                Бүртгүүлсэн:{state1}
+                                            </div>
+                                            <div className='m-25 px-1 p-25' style={{ borderLeft: `8px solid ${'rgba(40,199,11, 1)'}`, borderTop: '5px solid transparent', borderBottom: '5px solid transparent' }}>
+                                                Тэнцсэн:{state2}
+                                            </div>
+                                            <div className='m-25 px-1 p-25' style={{ borderLeft: `8px solid ${'#EA5455'}`, borderTop: '5px solid transparent', borderBottom: '5px solid transparent' }}>
+                                                Тэнцээгүй:{state3}
+                                            </div>
+                                        </PopoverBody>
+                                    </Popover>
+                                </>
+                            }
+                        </div>
                     </div>
                     <div className='shadow border rounded-3 m-1 mx-25 p-1'>
                         <Label className='p-50'>
@@ -98,8 +152,8 @@ function StateModal({ stateModalHandler, stateModal, selectedStudents, stateop, 
                     {errors?.state_description && <FormFeedback className='d-block'>{errors?.state_description?.message}</FormFeedback>}
                     </div>
                     <div className='text-center my-50'>
-                        <Button color='primary' type='submit'>Хадгалах</Button>
-                        <Button onClick={() => stateModalHandler()}>Гарах</Button>
+                        <Button className='m-50' color='primary' type='submit'>Хадгалах</Button>
+                        <Button className='m-50 ' onClick={() => stateModalHandler()}>Гарах</Button>
                     </div>
                 </Form>
             </ModalBody>
