175affdd30cf8b91541f8341b3fd8b125fe94d40
Foxllik <yukionna697@gmail.com>
2024-03-29 21:21:32 +0800

    zasvar

--- backend/apps/elselt/views.py
+++ backend/apps/elselt/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import permission_classes
 from rest_framework.filters import SearchFilter
 
 from django.core.mail import send_mail
+from django.core.mail import send_mass_mail
 from django.template.loader import render_to_string
 from django.db import transaction
 from django.db.models import F, Subquery, OuterRef, Count
@@ -541,37 +542,57 @@ class AdmissionUserEmailAPIView(
 
                 create_email_info = []
 
-                for value in data["students"]:
-                    create_email_info.append(
-                        EmailInfo(
-                            user = ElseltUser.objects.get(id=value),
-                            message = html_body,
-                            send_user = User.objects.get(id=user.id),
-                        )
-                    )
+                # for value in data["students"]:
+                #     create_email_info.append(
+                #         EmailInfo(
+                #             user_id = value,
+                #             message = html_body,
+                #             send_user_id = user.id,
+                #         )
+                #     )
 
-                self.queryset.bulk_create(create_email_info)
-
-                school_info = Employee.objects.filter(user=user.id).first().org
+                # self.queryset.bulk_create(create_email_info)
 
                 config = {
-                    "email_password": school_info.email_password,
-                    "email_port": school_info.email_port,
-                    "email_host": school_info.email_host,
-                    "email_use_tsl": True,
+                    "email_password": user.employee.org.email_password,
+                    "email_port": user.employee.org.email_port,
+                    "email_host": user.employee.org.email_host,
+                    "email_use_tsl": user.employee.org.email_use_tls,
                 }
 
-                send_mail(
-                    subject = 'Элсэлт',
-                    message = 'Элсэлтийн дүн гарлаа',
-                    from_email = school_info.email_host_user,
-                    recipient_list = data["email_list"],
-                    connection = make_connection(school_info.email_host_user, config),
-                    html_message = html_body
+                messages = [('Элсэлт', '<b>Элсэлтийн дүн гарлаа</b>', user.employee.org.email_host_user, [recipient]) for recipient in data["email_list"]]
+
+                send_mass_mail(
+                    datatuple = messages,
+                    connection = make_connection(user.employee.org.email_host_user, config)
                 )
+
+                # send_mail(
+                #     subject = 'Элсэлт',
+                #     message = 'Элсэлтийн дүн гарлаа',
+                #     from_email = user.employee.org.email_host_user,
+                #     recipient_list = data["email_list"],
+                #     html_message = html_body,
+                #     connection = make_connection(user.employee.org.email_host_user, config),
+                # )
+
+
+                # for mail in data["email_list"]:
+
+
+                #     send_mail(
+                #         subject = 'Элсэлт',
+                #         message = 'Элсэлтийн дүн гарлаа',
+                #         from_email = user.employee.org.email_host_user,
+                #         # recipient_list = data["email_list"],
+                #         recipient_list = [mail],
+                #         connection = make_connection(user.employee.org.email_host_user, config),
+                #         html_message = html_body
+                #     )
 
 
         except Exception as e:
+            print('e', e)
             transaction.savepoint_rollback(sid)
             return request.send_error("ERR_002", e.__str__)
         return request.send_info('INF_001')
