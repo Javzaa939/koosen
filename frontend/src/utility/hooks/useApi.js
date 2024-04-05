f75f3f3daa1cac364151f97d9d898e68ab238792
gracexnn <102289404+gracexnn@users.noreply.github.com>
2024-04-05 12:58:43 +0800

    post request туршилт
    Үзлэгийн хариу туршилтийн хэсэг

--- frontend/src/utility/hooks/useApi.js
+++ frontend/src/utility/hooks/useApi.js
@@ -1642,6 +1642,7 @@ function useApi(isDisplay=false) {
 		health: {
 			anhan:{
 				get: (limit, page, sort, search, lesson_year_id, profession_id, unit1_id, gender, state, gpa_state) => instance.get(`/elselt/health/anhan/?page=${page}&limit=${limit}&sorting=${sort}&search=${search}&lesson_year_id=${lesson_year_id}&profession_id=${profession_id}&unit1_id=${unit1_id}&gender=${gender}&state=${state}&gpa_state=${gpa_state}`),
+				post: (cdata) => instance.post(`/elselt/health/anhan/`, cdata)
 			},
 		}
 	}
--- frontend/src/views/Elselt/Health/AnhanShat/AddModal/validateSchema.jsx
+++ frontend/src/views/Elselt/Health/AnhanShat/AddModal/validateSchema.jsx
@@ -1,7 +1,7 @@
 import * as Yup from 'yup'
 
 export const validateSchema = Yup.object().shape({
-    height: Yup.string()
-        .trim()
-        .required('Хоосон байна'),
+    // height: Yup.string()
+    //     .trim()
+    //     .required('Хоосон байна'),
 });
