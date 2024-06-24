9b7cde3b491c1f60be836bf4b3d97f279326617a
Nak-1 <natsagdorj.tu@gmail.com>
2024-06-24 11:36:22 +0800

    date time

--- frontend/src/views/Calendar/AccessHistory.jsx
+++ frontend/src/views/Calendar/AccessHistory.jsx
@@ -1,15 +1,15 @@
 import { ListItem } from "@mui/material";
 import React, { useContext, useEffect, useState } from "react";
 import {
-  Card,
-  CardHeader,
-  CardTitle,
-  Col,
-  ListGroup,
-  ListGroupItem,
-  Modal,
-  ModalBody,
-  ModalHeader,
+    Card,
+    CardHeader,
+    CardTitle,
+    Col,
+    ListGroup,
+    ListGroupItem,
+    Modal,
+    ModalBody,
+    ModalHeader,
 } from "reactstrap";
 import { RiComputerLine } from "react-icons/ri";
 import { RxDotsVertical } from "react-icons/rx";
@@ -18,104 +18,113 @@ import useApi from "@src/utility/hooks/useApi";
 import useLoader from "@src/utility/hooks/useLoader";
 
 const AccessHistory = (props) => {
-  const [accessHistories, setAccessHistories] = useState([]);
-  const [showAll, setShowAll] = useState(false);
+    const [accessHistories, setAccessHistories] = useState([]);
+    const [showAll, setShowAll] = useState(false);
 
-  const { fetchData, isLoading, Loader } = useLoader({});
-  const accessHistoryAPI = useApi().calendar;
+    const { fetchData, isLoading, Loader } = useLoader({});
+    const accessHistoryAPI = useApi().calendar;
 
-  async function getAccess() {
-    const { success, data } = await fetchData(
-      accessHistoryAPI.getAccessHistory()
-    );
-    if (success) {
-      setAccessHistories(data);
+    async function getAccess() {
+        const { success, data } = await fetchData(
+            accessHistoryAPI.getAccessHistory()
+        );
+        if (success) {
+            setAccessHistories(data);
+        }
     }
-  }
 
-  useEffect(() => {
-    getAccess();
-  }, []);
+    useEffect(() => {
+        getAccess();
+    }, []);
 
-  const handleSeeMore = () => {
-    setShowAll(true);
-  };
-  const handleClose = () => {
-    setShowAll(false);
-  };
+    const handleSeeMore = () => {
+        setShowAll(true);
+    };
+    const handleClose = () => {
+        setShowAll(false);
+    };
 
-  const Item = (props) => {
+    const Item = (props) => {
+        const name = props.event.device_name
+        const date = props.event.in_time.slice(0, 10);
+        const time = props.event.in_time.slice(11, 19);
+        return (
+            <ListGroupItem
+                key={props.index}
+                divider
+                style={{
+                    fontSize: "11px",
+                    height: "65px",
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                }}
+            >
+                <RiComputerLine size={25} />
+                <Col className="align-middle ms-50">
+                    <h6 className="align-middle">{name}</h6>
+                    <span className="align-middle">
+                        {date} {time}
+                    </span>
+                    <div className="align-middle">{props.event.ip}</div>
+                </Col>
+            </ListGroupItem>
+        );
+    };
     return (
-      <ListGroupItem
-        key={props.index}
-        divider
-        style={{
-          fontSize: "12px",
-          height: "60px",
-          display: "flex",
-          flexDirection: "row",
-        }}
-      >
-        <RiComputerLine size={30} />
-        <Col className="align-middle ms-50">
-          <span className="align-middle ms-50">{props.event.in_time}</span>
-          <div className="align-middle ms-50">{props.event.ip}</div>
-        </Col>
-      </ListGroupItem>
+        <Card>
+            <CardHeader>
+                <CardTitle tag={"h5"}>Xандалтийн түүх</CardTitle>
+            </CardHeader>
+            <ListGroup>
+                {(showAll
+                    ? accessHistories.slice(0, 5)
+                    : accessHistories.slice(0, 5)
+                ).map((event, index) => (
+                    <Item key={index} event={event} />
+                ))}
+                {accessHistories.length > 5 && !showAll && (
+                    <ListItem>
+                        <RxDotsVertical
+                            size={20}
+                            className="ms-50"
+                            onClick={handleSeeMore}
+                        />
+                        <span className="align-middle ms-50">
+                            Дэлгэрэнгүй үзэх
+                        </span>
+                    </ListItem>
+                )}
+            </ListGroup>
+            <Modal
+                isOpen={showAll}
+                onRequestClose={handleClose}
+                style={{
+                    height: "50vh",
+                    content: {
+                        maxHeight: "50vh",
+                        overflow: "auto",
+                    },
+                }}
+            >
+                <ModalHeader
+                    style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                    <RxCross2
+                        onClick={handleClose}
+                        size={30}
+                        style={{ position: "fixed", zIndex: 1 }}
+                    />
+                </ModalHeader>
+                <ModalBody style={{ marginTop: "10px" }}>
+                    <ListGroup>
+                        {accessHistories.map((event, index) => (
+                            <Item key={index} event={event} />
+                        ))}
+                    </ListGroup>
+                </ModalBody>
+            </Modal>
+        </Card>
     );
-  };
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle tag={"h5"}>Xандалтийн түүх</CardTitle>
-      </CardHeader>
-      <ListGroup>
-        {(showAll
-          ? accessHistories.slice(0, 5)
-          : accessHistories.slice(0, 5)
-        ).map((event, index) => (
-          <Item key={index} event={event} />
-        ))}
-        {accessHistories.length > 5 && !showAll && (
-          <ListItem>
-            <RxDotsVertical
-              size={20}
-              className="ms-50"
-              onClick={handleSeeMore}
-            />
-            <span className="align-middle ms-50">Дэлгэрэнгүй үзэх</span>
-          </ListItem>
-        )}
-      </ListGroup>
-      <Modal
-        isOpen={showAll}
-        onRequestClose={handleClose}
-        style={{
-          height: "50vh",
-          content: {
-            maxHeight: "50vh",
-            overflow: "auto",
-          },
-        }}
-      >
-        <ModalHeader
-          style={{ display: "flex", justifyContent: "space-between" }}
-        >
-          <RxCross2
-            onClick={handleClose}
-            size={30}
-            style={{ position: "fixed", zIndex: 1 }}
-          />
-        </ModalHeader>
-        <ModalBody style={{ marginTop: "10px" }}>
-          <ListGroup>
-            {accessHistories.map((event, index) => (
-              <Item key={index} event={event} />
-            ))}
-          </ListGroup>
-        </ModalBody>
-      </Modal>
-    </Card>
-  );
 };
 export default AccessHistory;
--- frontend/src/views/Calendar/index.jsx
+++ frontend/src/views/Calendar/index.jsx
@@ -2,13 +2,11 @@
 import { Fragment, useState, useEffect } from "react";
 import { Book, User } from "react-feather";
 import {
-  Card,
-  CardBody,
-  CardTitle,
-  Row,
-  Col,
-  ListGroup,
-  ListGroupItem,
+    Card,
+    CardBody,
+    CardTitle,
+    Row,
+    Col,
 } from "reactstrap";
 import classnames from "classnames";
 
@@ -30,212 +28,222 @@ import StatsHorizontal from "@components/widgets/stats/StatsHorizontal";
 import "./style.scss";
 import AccessHistory from "./AccessHistory";
 const CalendarComponent = () => {
-  const blankEvent = {
-    title: "",
-    start: "",
-    end: "",
-  };
+    const blankEvent = {
+        title: "",
+        start: "",
+        end: "",
+    };
 
-  const { fetchData } = useLoader({});
+    const { fetchData } = useLoader({});
 
-  // ** states
-  const [addSidebarOpen, setAddSidebarOpen] = useState(false);
-  const [leftSidebarOpen, setLeftSidebarOpen] = useState(false);
-  const [datas, setDatas] = useState([]);
-  const [edit_id, setEditId] = useState("");
-  const [is_new, setNew] = useState(false);
-  const [searchChecked, setSearchChecked] = useState([]);
-  const [dates, setDates] = useState(blankEvent);
-  const [active_week, setActiveWeek] = useState(1);
-  const [info, setInfo] = useState({
-    salbar_data: [],
-  });
-  const [datas1, setDatas1] = useState([]);
+    // ** states
+    const [addSidebarOpen, setAddSidebarOpen] = useState(false);
+    const [leftSidebarOpen, setLeftSidebarOpen] = useState(false);
+    const [datas, setDatas] = useState([]);
+    const [edit_id, setEditId] = useState("");
+    const [is_new, setNew] = useState(false);
+    const [searchChecked, setSearchChecked] = useState([]);
+    const [dates, setDates] = useState(blankEvent);
+    const [active_week, setActiveWeek] = useState(1);
+    const [info, setInfo] = useState({
+        salbar_data: [],
+    });
+    const [datas1, setDatas1] = useState([]);
 
-  // ** Hooks
-  const [isRtl] = useRTL();
+    // ** Hooks
+    const [isRtl] = useRTL();
 
-  // Api
-  const calendarListApi = useApi().calendar;
-  const parentschoolApi = useApi().calendarCard;
-  const calendarNewsApi = useApi().calendarNews;
+    // Api
+    const calendarListApi = useApi().calendar;
+    const parentschoolApi = useApi().calendarCard;
+    const calendarNewsApi = useApi().calendarNews;
 
-  // ** AddEventSidebar Toggle Function
-  const handleAddEventSidebar = () => {
-    setAddSidebarOpen(!addSidebarOpen);
-    if (addSidebarOpen) {
-      setEditId("");
+    // ** AddEventSidebar Toggle Function
+    const handleAddEventSidebar = () => {
+        setAddSidebarOpen(!addSidebarOpen);
+        if (addSidebarOpen) {
+            setEditId("");
+        }
+    };
+
+    // ** LeftSidebar Toggle Function
+    const toggleSidebar = (val) => setLeftSidebarOpen(val);
+
+    async function getDatas() {
+        const { success, data } = await fetchData(
+            calendarListApi.get(searchChecked)
+        );
+        if (success) {
+            setActiveWeek(data?.active_week);
+
+            if (data.datas) {
+                var calendarDatas = data.datas;
+
+                calendarDatas.forEach((data) => {
+                    if (data.action_type == VOLUNTEER_ACTION_TYPE) {
+                        data["title"] = "ОНА" + " " + data?.title;
+                        data["color"] = "#e9ff70";
+                    } else {
+                        data["textColor"] = "white";
+                    }
+                });
+
+                setDatas(calendarDatas);
+            }
+        }
     }
-  };
 
-  // ** LeftSidebar Toggle Function
-  const toggleSidebar = (val) => setLeftSidebarOpen(val);
+    useEffect(() => {
+        getDatas();
+    }, [searchChecked]);
 
-  async function getDatas() {
-    const { success, data } = await fetchData(
-      calendarListApi.get(searchChecked)
+    // Хуанли дах card-уудын мэдээлэл
+    async function getDatas1() {
+        const { success, data } = await fetchData(parentschoolApi.get());
+        if (success) {
+            setInfo(data);
+        }
+    }
+
+    useEffect(() => {
+        getDatas1();
+    }, []);
+
+    // Хуанли дах мэдээлэллийн хэсэг
+
+    async function getDatas2() {
+        const { success, data } = await fetchData(calendarNewsApi.get());
+        if (success) {
+            setDatas1(data);
+        }
+    }
+
+    useEffect(() => {
+        getDatas2();
+    }, []);
+
+    return (
+        <Fragment>
+            <div className=" pt-0">
+                <Row>
+                    <Col lg="3" sm="6">
+                        <StatsHorizontal
+                            color="warning"
+                            statTitle="Нийт хөтөлбөрийн тоо"
+                            icon={<Book size={20} />}
+                            renderStats={
+                                <h3 className="fw-bolder mb-75">
+                                    {info?.total_profession}
+                                </h3>
+                            }
+                        />
+                    </Col>
+                    <Col lg="3" sm="6">
+                        <StatsHorizontal
+                            color="warning"
+                            statTitle="Нийт хичээлийн тоо"
+                            icon={<Book size={20} />}
+                            renderStats={
+                                <h3 className="fw-bolder mb-75">
+                                    {info?.total_studies}
+                                </h3>
+                            }
+                        />
+                    </Col>
+                    <Col lg="3" sm="6">
+                        <StatsHorizontal
+                            color="primary"
+                            statTitle="Нийт багшийн тоо"
+                            icon={<User size={20} />}
+                            renderStats={
+                                <h3 className="fw-bolder mb-75">
+                                    {info?.total_workers}
+                                </h3>
+                            }
+                        />
+                    </Col>
+                    <Col lg="3" sm="6">
+                        <StatsHorizontal
+                            color="danger"
+                            statTitle="Нийт суралцагчдын тоо"
+                            icon={<User size={20} />}
+                            renderStats={
+                                <h3 className="fw-bolder mb-75">
+                                    {info?.total_students}
+                                </h3>
+                            }
+                        />
+                    </Col>
+                </Row>
+            </div>
+
+            <div className="app-calendar overflow-hidden border">
+                <Row className="g-0">
+                    <Col
+                        id="app-calendar-sidebar"
+                        className={classnames(
+                            "col app-calendar-sidebar flex-grow-0 overflow-hidden d-flex flex-column",
+                            {
+                                show: leftSidebarOpen,
+                            }
+                        )}
+                    >
+                        <SidebarLeft
+                            toggleSidebar={toggleSidebar}
+                            handleAddEventSidebar={handleAddEventSidebar}
+                            searchValue={setSearchChecked}
+                            setNew={setNew}
+                            active_week={active_week}
+                        />
+                    </Col>
+                    <Col className="position-relative">
+                        <Calendar
+                            isRtl={isRtl}
+                            toggleSidebar={toggleSidebar}
+                            handleAddEventSidebar={handleAddEventSidebar}
+                            eventDatas={datas}
+                            setEditId={setEditId}
+                            setNew={setNew}
+                            blankEvent={blankEvent}
+                            getDates={setDates}
+                        />
+                    </Col>
+
+                    <Col lg="3" sm="9" className="m-2">
+                        <Card className="text-center">
+                            <CardBody>
+                                <CardTitle tag="h3">
+                                    Сүүлд нэмэгдсэн мэдээ, мэдээлэл
+                                </CardTitle>
+                                {datas1.length > 0 ? (
+                                    <SwiperFade datas={datas1} />
+                                ) : (
+                                    <h5>Мэдээлэл байхгүй байна!</h5>
+                                )}
+                            </CardBody>
+                        </Card>
+                        <AccessHistory />
+                    </Col>
+
+                    <div
+                        className={classnames("body-content-overlay", {
+                            show: leftSidebarOpen === true,
+                        })}
+                        onClick={() => toggleSidebar(false)}
+                    ></div>
+                </Row>
+            </div>
+
+            <AddEventSidebar
+                open={addSidebarOpen}
+                handleAddEventSidebar={handleAddEventSidebar}
+                refreshDatas={getDatas}
+                editId={edit_id}
+                is_new={is_new}
+                dates={dates}
+            />
+        </Fragment>
     );
-    if (success) {
-      setActiveWeek(data?.active_week);
-
-      if (data.datas) {
-        var calendarDatas = data.datas;
-
-        calendarDatas.forEach((data) => {
-          if (data.action_type == VOLUNTEER_ACTION_TYPE) {
-            data["title"] = "ОНА" + " " + data?.title;
-            data["color"] = "#e9ff70";
-          } else {
-            data["textColor"] = "white";
-          }
-        });
-
-        setDatas(calendarDatas);
-      }
-    }
-  }
-
-  useEffect(() => {
-    getDatas();
-  }, [searchChecked]);
-
-  // Хуанли дах card-уудын мэдээлэл
-  async function getDatas1() {
-    const { success, data } = await fetchData(parentschoolApi.get());
-    if (success) {
-      setInfo(data);
-    }
-  }
-
-  useEffect(() => {
-    getDatas1();
-  }, []);
-
-  // Хуанли дах мэдээлэллийн хэсэг
-
-  async function getDatas2() {
-    const { success, data } = await fetchData(calendarNewsApi.get());
-    if (success) {
-      setDatas1(data);
-    }
-  }
-
-  useEffect(() => {
-    getDatas2();
-  }, []);
-
-  return (
-    <Fragment>
-      <div className=" pt-0">
-        <Row>
-          <Col lg="3" sm="6">
-            <StatsHorizontal
-              color="warning"
-              statTitle="Нийт хөтөлбөрийн тоо"
-              icon={<Book size={20} />}
-              renderStats={
-                <h3 className="fw-bolder mb-75">{info?.total_profession}</h3>
-              }
-            />
-          </Col>
-          <Col lg="3" sm="6">
-            <StatsHorizontal
-              color="warning"
-              statTitle="Нийт хичээлийн тоо"
-              icon={<Book size={20} />}
-              renderStats={
-                <h3 className="fw-bolder mb-75">{info?.total_studies}</h3>
-              }
-            />
-          </Col>
-          <Col lg="3" sm="6">
-            <StatsHorizontal
-              color="primary"
-              statTitle="Нийт багшийн тоо"
-              icon={<User size={20} />}
-              renderStats={
-                <h3 className="fw-bolder mb-75">{info?.total_workers}</h3>
-              }
-            />
-          </Col>
-          <Col lg="3" sm="6">
-            <StatsHorizontal
-              color="danger"
-              statTitle="Нийт суралцагчдын тоо"
-              icon={<User size={20} />}
-              renderStats={
-                <h3 className="fw-bolder mb-75">{info?.total_students}</h3>
-              }
-            />
-          </Col>
-        </Row>
-      </div>
-
-      <div className="app-calendar overflow-hidden border">
-        <Row className="g-0">
-          <Col
-            id="app-calendar-sidebar"
-            className={classnames(
-              "col app-calendar-sidebar flex-grow-0 overflow-hidden d-flex flex-column",
-              {
-                show: leftSidebarOpen,
-              }
-            )}
-          >
-            <SidebarLeft
-              toggleSidebar={toggleSidebar}
-              handleAddEventSidebar={handleAddEventSidebar}
-              searchValue={setSearchChecked}
-              setNew={setNew}
-              active_week={active_week}
-            />
-          </Col>
-          <Col className="position-relative">
-            <Calendar
-              isRtl={isRtl}
-              toggleSidebar={toggleSidebar}
-              handleAddEventSidebar={handleAddEventSidebar}
-              eventDatas={datas}
-              setEditId={setEditId}
-              setNew={setNew}
-              blankEvent={blankEvent}
-              getDates={setDates}
-            />
-          </Col>
-
-          <Col lg="3" sm="9" className="m-2">
-            <Card className="text-center">
-              <CardBody>
-                <CardTitle tag="h3">Сүүлд нэмэгдсэн мэдээ, мэдээлэл</CardTitle>
-                {datas1.length > 0 ? (
-                  <SwiperFade datas={datas1} />
-                ) : (
-                  <h5>Мэдээлэл байхгүй байна!</h5>
-                )}
-              </CardBody>
-            </Card>
-            <AccessHistory />
-          </Col>
-
-          <div
-            className={classnames("body-content-overlay", {
-              show: leftSidebarOpen === true,
-            })}
-            onClick={() => toggleSidebar(false)}
-          ></div>
-        </Row>
-      </div>
-
-      <AddEventSidebar
-        open={addSidebarOpen}
-        handleAddEventSidebar={handleAddEventSidebar}
-        refreshDatas={getDatas}
-        editId={edit_id}
-        is_new={is_new}
-        dates={dates}
-      />
-    </Fragment>
-  );
 };
 
 export default CalendarComponent;
