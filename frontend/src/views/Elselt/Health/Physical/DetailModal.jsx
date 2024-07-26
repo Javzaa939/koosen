fc0a9d60f65362b13587348bcd05f42b69a45477
Nak-1 <natsagdorj.tu@gmail.com>
2024-07-26 11:57:35 +0800

    bolson

--- frontend/src/views/Elselt/Health/Physical/DetailModal.jsx
+++ frontend/src/views/Elselt/Health/Physical/DetailModal.jsx
@@ -165,16 +165,23 @@ export default function DetailModal({ isOpen, handleModal, datas, file_name, err
                                                     </thead>
                                                     <tbody>
                                                         {
-                                                            data?.map((row, idx) =>
-                                                                <tr key={idx}>
-                                                                    <td className='bg-transparent'>{idx + 1}</td>
-                                                                    {
-                                                                        headers.map((column, idx2) => (
-                                                                            <td key={`score-${idx2}${idx}`} className='bg-transparent'>{row[column.key]}</td>
-                                                                        )
-                                                                        )}
-                                                                </tr>
-                                                            )}
+                                                            data?.map((row, idx) => {
+                                                                if(row.state == 1) row.state = 'Бүртгүүлсэн'
+                                                                if(row.state == 2) row.state = 'Тэнцсэн'
+                                                                if(row.state == 3) row.state = 'Тэнцээгүй'
+                                                                return (
+                                                                        <tr key={idx}>
+                                                                            <td className='bg-transparent'>{idx + 1}</td>
+                                                                            {
+                                                                                headers.map((column, idx2) => (
+                                                                                    <td key={`score-${idx2}${idx}`} className='bg-transparent'>{row[column.key]}</td>
+                                                                                ))
+                                                                            }
+                                                                        </tr>
+                                                                    )
+                                                                }
+                                                            )
+                                                        }
                                                     </tbody>
                                                 </Table>
                                             </div>
