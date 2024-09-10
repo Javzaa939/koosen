4aed4323e2a471aeadb048636f38656eed7c56ba
duo456 <duo456@gmail.com>
2024-09-10 19:25:09 +0800

    grid list switch, searching added

---
+++ frontend/src/views/OnlineLesson/AllLessons/helpers/index.jsx
@@ -0,0 +1,80 @@
+import { t } from "i18next";
+import { Book, Clipboard } from "react-feather";
+import { Link } from "react-router-dom";
+import { Badge, UncontrolledTooltip } from "reactstrap";
+// Хүснэгтийн баганууд
+export function getColumns (currentPage, rowsPerPage, datas) {
+
+    const page_count = Math.ceil(datas.length / rowsPerPage)
+
+    /** Сонгосон хуудасны тоо датаны тооноос их болсон үед хуудаслалт 1-ээс эхлэнэ */
+    if (currentPage > page_count) {
+        currentPage = 1
+    }
+
+    const columns = [
+		{
+			name: "№",
+			selector: (row, index) => (currentPage-1) * rowsPerPage + index + 1,
+			maxWidth: "30px",
+			center: true
+		},
+		{
+			name: `${t('Хичээлийн нэр')}`,
+			selector: (row) => row?.lesson_name,
+            sortable: true,
+			minWidth: "280px",
+			center: true
+		},
+		{
+			name: `${t('Багш')}`,
+            cell: (row) => row?.teacher?.full_name,
+			minWidth: "280px",
+			center: true
+		},
+		{
+			name: `${t('Оюутны тоо')}`,
+            cell: (row) => row?.student_count,
+			minWidth: "280px",
+			center: true
+		},
+		{
+			name: `${t('Хичээл')}`,
+            cell: (row) => row?.total_homeworks_and_exams.week_count,
+			minWidth: "280px",
+			center: true
+		},
+		{
+			name: `${t('Даалгавар')}`,
+            cell: (row) => row?.total_homeworks_and_exams.homework_count,
+			minWidth: "280px",
+			center: true
+		},
+		{
+			name: `${t('Шалгалт')}`,
+            cell: (row) => row?.total_homeworks_and_exams.challenge_count,
+			minWidth: "280px",
+			center: true
+		},
+		{
+			name: `${t('Үйлдэл')}`,
+			selector: (row) => {
+				return (
+					<>
+						<Link to={`/online_lesson/${row.id}`} id={`detail${row.id}`}>
+							<Badge color="light-info" pill><Book  width={"15px"} /></Badge>
+						</Link>
+						<UncontrolledTooltip placement="top" target={`detail${row.id}`} >
+							Дэлгэрэнгүй
+						</UncontrolledTooltip>
+					</>
+				)
+			},
+            sortable: false,
+			center: true
+		},
+	]
+
+    return columns
+
+}
--- frontend/src/views/OnlineLesson/AllLessons/index.jsx
+++ frontend/src/views/OnlineLesson/AllLessons/index.jsx
@@ -4,13 +4,17 @@ import { GoDotFill } from "react-icons/go";
 import ReactPaginate from "react-paginate";
 import { Link } from "react-router-dom";
 
-import { Card, CardBody, CardHeader, Col, Row, Button, Modal, UncontrolledTooltip, Badge, Alert } from "reactstrap";
+import { Card, CardBody, CardHeader, Col, Row, Button, Modal, UncontrolledTooltip, Label, Input } from "reactstrap";
 import useModal from '@hooks/useModal';
 import useApi from "@src/utility/hooks/useApi";
 import useLoader from '@hooks/useLoader'
 
 import AddLessonForm from "./AddLessonForm";
-import { ChevronsRight, HelpCircle, X } from 'react-feather'
+import { ChevronDown, ChevronsRight, Grid, List, X } from 'react-feather'
+import DataTable from "react-data-table-component";
+import { getPagination } from "@src/utility/Utils";
+import { getColumns } from './helpers'
+import { t } from "i18next";
 
 function AllLessons({ lessons, getLessons }) {
     const [currentPage, setCurrentPage] = useState(1);
@@ -45,6 +49,44 @@ function AllLessons({ lessons, getLessons }) {
         }
     }
 
+    const [view, setView] = useState('grid');
+	const [searchValue, setSearchValue] = useState("");
+	const [filteredData, setFilteredData] = useState([]);
+
+	// Хайлт хийх үед ажиллах хэсэг
+	const handleFilter = (e) => {
+		var updatedData = [];
+		const value = e.target.value.trimStart();
+
+		setSearchValue(value);
+
+		if (value.length) {
+			updatedData = lessons.filter((item) => {
+                console.log(item)
+				const startsWith =
+					item.teacher.full_name.toString().toLowerCase().startsWith(value.toString().toLowerCase()) ||
+					item.lesson_name.toString().toLowerCase().startsWith(value.toString().toLowerCase())
+
+				const includes =
+					item.teacher.full_name.toString().toLowerCase().includes(value.toString().toLowerCase()) ||
+					item.lesson_name.toString().toLowerCase().includes(value.toString().toLowerCase())
+
+				if (startsWith) {
+					return startsWith;
+				}
+				else if (!startsWith && includes) {
+					return includes;
+				}
+				else {
+					return null;
+				}
+			});
+
+			setFilteredData(updatedData);
+			setSearchValue(value);
+		}
+	};
+
     return (
         <Fragment>
             <Card>
@@ -57,6 +99,13 @@ function AllLessons({ lessons, getLessons }) {
                             sm={12}
                         >
                             <div>
+                                <Button
+                                    className="px-1 me-1"
+                                    style={{paddingTop: 5, paddingBottom: 5}}
+                                    onClick={() => setView(view === 'grid' ? 'table' : 'grid')}
+                                >
+                                    {view === 'grid' ? <List size={20} /> : <Grid size={20} />}
+                                </Button>
                                 <Button color="primary" onClick={toggle}>
                                     Хичээл үүсгэх
                                 </Button>
@@ -74,104 +123,153 @@ function AllLessons({ lessons, getLessons }) {
                     </Row>
                 </CardHeader>
                 <CardBody className="">
-                    <Row>
-                        {displayedLessons.map((lesson, index) => (
-                            <Col key={index} xl={3} md={6} className="mb-1">
-                                <div className="border rounded-lg shadow">
-                                    <CardBody>
-                                        <div className="d-flex justify-content-between mb-1">
-                                            <h6 className="">{lesson?.lesson_name}</h6>
-                                            <div>
-                                                <X
-                                                    color='red'
-                                                    size={15}
-                                                    id={`delete${index}`}
-                                                    onClick={() => showWarning({
-                                                        header: {
-                                                            title: 'Устгах үйлдэл',
-                                                        },
-                                                        question: 'Та энэхүү хичээлийг устгахдаа итгэлтэй байна уу?',
-                                                        onClick: () => deleteLesson(lesson.id),
-                                                        btnText: 'Устгах',
-                                                    })}
-                                                />
-                                                <UncontrolledTooltip placement='top' target={`delete${index}`} >{'Устгах'}</UncontrolledTooltip>
-                                            </div>
-                                        </div>
-                                        <h6 className="mb-1">{'Багш:'}<span className="ms-1">{lesson?.teacher?.full_name}</span></h6>
-                                        <h6 className="mb-1">{'Оюутны тоо:'}<span className="ms-1">{lesson?.student_count}</span></h6>
-                                        <div>
-                                            <div className="flex flex-md-row flex-column">
+                {view === 'grid' ?
+                    <>
+                        <Row>
+                            {displayedLessons.map((lesson, index) => (
+                                <Col key={index} xl={3} md={6} className="mb-1">
+                                    <div className="border rounded-lg shadow">
+                                        <CardBody>
+                                            <div className="d-flex justify-content-between mb-1">
+                                                <h6 className="">{lesson?.lesson_name}</h6>
                                                 <div>
-                                                    <div className="d-flex flex-column flex-md-row">
-                                                        <Row className="w-100">
-                                                            <Col xs={9}>
-                                                                {" "}
-                                                                <GoDotFill style={{ color: "#14B8A6", fontSize: "2rem" }} />
-                                                                Хичээл
-                                                            </Col>
-                                                            <Col xs={3}>{lesson?.total_homeworks_and_exams.week_count}</Col>
-                                                        </Row>
-                                                    </div>
-                                                    <div className="d-flex flex-column flex-md-row">
-                                                        <Row className="w-100">
-                                                            <Col xs={9}>
-                                                                {" "}
-                                                                <GoDotFill style={{ color: "#3B82F6", fontSize: "2rem" }} />
-                                                                Даалгавар
-                                                            </Col>
-                                                            <Col xs={3}>{lesson?.total_homeworks_and_exams.homework_count}</Col>
-                                                        </Row>
-                                                    </div>
-                                                    <div className="d-flex flex-column flex-md-row">
-                                                        <Row className="w-100">
-                                                            <Col xs={9}>
-                                                                {" "}
-                                                                <GoDotFill style={{ color: "#6366F1", fontSize: "2rem" }} />
-                                                                Шалгалт
-                                                            </Col>
-                                                            <Col xs={3}>{lesson?.total_homeworks_and_exams.challenge_count}</Col>
-                                                        </Row>
+                                                    <X
+                                                        color='red'
+                                                        size={15}
+                                                        id={`delete${index}`}
+                                                        onClick={() => showWarning({
+                                                            header: {
+                                                                title: 'Устгах үйлдэл',
+                                                            },
+                                                            question: 'Та энэхүү хичээлийг устгахдаа итгэлтэй байна уу?',
+                                                            onClick: () => deleteLesson(lesson.id),
+                                                            btnText: 'Устгах',
+                                                        })}
+                                                    />
+                                                    <UncontrolledTooltip placement='top' target={`delete${index}`} >{'Устгах'}</UncontrolledTooltip>
+                                                </div>
+                                            </div>
+                                            <h6 className="mb-1">{'Багш:'}<span className="ms-1">{lesson?.teacher?.full_name}</span></h6>
+                                            <h6 className="mb-1">{'Оюутны тоо:'}<span className="ms-1">{lesson?.student_count}</span></h6>
+                                            <div>
+                                                <div className="flex flex-md-row flex-column">
+                                                    <div>
+                                                        <div className="d-flex flex-column flex-md-row">
+                                                            <Row className="w-100">
+                                                                <Col xs={9}>
+                                                                    {" "}
+                                                                    <GoDotFill style={{ color: "#14B8A6", fontSize: "2rem" }} />
+                                                                    Хичээл
+                                                                </Col>
+                                                                <Col xs={3}>{lesson?.total_homeworks_and_exams.week_count}</Col>
+                                                            </Row>
+                                                        </div>
+                                                        <div className="d-flex flex-column flex-md-row">
+                                                            <Row className="w-100">
+                                                                <Col xs={9}>
+                                                                    {" "}
+                                                                    <GoDotFill style={{ color: "#3B82F6", fontSize: "2rem" }} />
+                                                                    Даалгавар
+                                                                </Col>
+                                                                <Col xs={3}>{lesson?.total_homeworks_and_exams.homework_count}</Col>
+                                                            </Row>
+                                                        </div>
+                                                        <div className="d-flex flex-column flex-md-row">
+                                                            <Row className="w-100">
+                                                                <Col xs={9}>
+                                                                    {" "}
+                                                                    <GoDotFill style={{ color: "#6366F1", fontSize: "2rem" }} />
+                                                                    Шалгалт
+                                                                </Col>
+                                                                <Col xs={3}>{lesson?.total_homeworks_and_exams.challenge_count}</Col>
+                                                            </Row>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div className="text-end mt-25">
-                                            <Link to={`/online_lesson/${lesson.id}`}>
-                                                дэлгэрэнгүй <ChevronsRight size={15}/>
-                                            </Link>
-                                        </div>
-                                    </CardBody>
-                                </div>
+                                            <div className="text-end mt-25">
+                                                <Link to={`/online_lesson/${lesson.id}`}>
+                                                    дэлгэрэнгүй <ChevronsRight size={15}/>
+                                                </Link>
+                                            </div>
+                                        </CardBody>
+                                    </div>
+                                </Col>
+                            ))}
+                        </Row>
+                        <Row className="d-flex justify-content-end mx-0 my-0" sm={12}>
+                            <Col
+                                className="d-flex align-items-center justify-content-end"
+                                md={6}
+                                sm={12}
+                            >
+                                <ReactPaginate
+                                    previousLabel={"←"}
+                                    nextLabel={"→"}
+                                    pageCount={totalPages}
+                                    onPageChange={handlePagination}
+                                    containerClassName={"pagination"}
+                                    activeClassName={"active"}
+                                    pageClassName={"page-item"}
+                                    pageLinkClassName={"page-link"}
+                                    previousClassName={"page-item"}
+                                    previousLinkClassName={"page-link"}
+                                    nextClassName={"page-item"}
+                                    nextLinkClassName={"page-link"}
+                                    breakClassName={"page-item"}
+                                    breakLinkClassName={"page-link"}
+                                    marginPagesDisplayed={2}
+                                    pageRangeDisplayed={5}
+                                />
                             </Col>
-                        ))}
-                    </Row>
-                    <Row className="d-flex justify-content-end mx-0 my-0" sm={12}>
-                        <Col
-                            className="d-flex align-items-center justify-content-end"
-                            md={6}
-                            sm={12}
-                        >
-                            <ReactPaginate
-                                previousLabel={"←"}
-                                nextLabel={"→"}
-                                pageCount={totalPages}
-                                onPageChange={handlePagination}
-                                containerClassName={"pagination"}
-                                activeClassName={"active"}
-                                pageClassName={"page-item"}
-                                pageLinkClassName={"page-link"}
-                                previousClassName={"page-item"}
-                                previousLinkClassName={"page-link"}
-                                nextClassName={"page-item"}
-                                nextLinkClassName={"page-link"}
-                                breakClassName={"page-item"}
-                                breakLinkClassName={"page-link"}
-                                marginPagesDisplayed={2}
-                                pageRangeDisplayed={5}
-                            />
-                        </Col>
-                    </Row>
+                        </Row>
+                    </>
+                :
+                    <>
+                        <Row>
+                            <Col className="datatable-search-text d-flex justify-content-start mt-1" md={6} sm={6}>
+                                <Label className="me-1 search-filter-title pt-50" for="search-input">
+                                    {t('Хайлт')}
+                                </Label>
+                                <Input
+                                    className="dataTable-filter mb-50"
+                                    type="text"
+                                    bsSize="sm"
+                                    id="search-input"
+                                    value={searchValue}
+                                    onChange={handleFilter}
+                                    placeholder={t('Хайх үг....')}
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <DataTable
+                                    pagination
+                                    className='react-dataTable'
+                                    progressComponent={(
+                                        <div className='my-2'>
+                                            <h5>{t('Түр хүлээнэ үү')}...</h5>
+                                        </div>
+                                    )}
+                                    noDataComponent={(
+                                        <div className="my-2">
+                                            <h5>{t('Өгөгдөл байхгүй байна')}</h5>
+                                        </div>
+                                    )}
+                                    columns={getColumns(currentPage, rowsPerPage, searchValue.length ? filteredData : lessons)}
+                                    sortIcon={<ChevronDown size={10} />}
+                                    paginationPerPage={rowsPerPage}
+                                    paginationDefaultPage={currentPage}
+                                    data={searchValue.length ? filteredData : displayedLessons}
+                                    paginationComponent={getPagination(handlePagination, currentPage, rowsPerPage, lessons?.length, searchValue, filteredData)}
+                                    fixedHeader
+                                    fixedHeaderScrollHeight='62vh'
+                                />
+                            </Col>
+                        </Row>
+                    </>
+                }
                 </CardBody>
             </Card>
 
