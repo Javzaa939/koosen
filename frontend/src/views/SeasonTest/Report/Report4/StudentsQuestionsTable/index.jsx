3e78d1a6b1a1de02f4ae1e7d54b5d40e963d1701
duo456 <duo456@gmail.com>
2024-12-29 17:29:10 +0800

    465-tooltip and table scrolling compatibiility added, refactored, word wrap and tooltip sizes adapted, etc

--- frontend/src/views/SeasonTest/Report/Report4/StudentsQuestionsTable/index.jsx
+++ frontend/src/views/SeasonTest/Report/Report4/StudentsQuestionsTable/index.jsx
@@ -1,5 +1,7 @@
+import { useState } from 'react'
 import './style.scss'
 import { useTranslation } from "react-i18next"
+import ReactDOM from "react-dom";
 
 function get_question_title_color(title) {
 	if (typeof title === 'string') {
@@ -12,77 +14,109 @@ function get_question_title_color(title) {
 	}
 }
 
-// TODO: add sorting
 export default function StudentsQuestionsTable({ data, aggregatedData, handleSort }) {
 	const { t } = useTranslation()
+	const [tooltipStyle, setTooltipStyle] = useState({});
+	const [visible, setVisible] = useState(false);
+	const [tooltipText, setTooltipText] = useState("");
+
+	const showTooltip = (e, text) => {
+		const { top, left, width, height } = e.currentTarget.getBoundingClientRect();
+
+		setTooltipStyle({
+			top: top + height,
+			left: left,
+		});
+
+		setTooltipText(text);
+		setVisible(true);
+	};
+
+	const hideTooltip = () => setVisible(false);
 
 	return (
-		// TODO: add table scroll. if <Table responsive~ used or overflow auto used then tooltip not showing somehow...
-		<table className='studentsQuestionsTable'>
-			<thead align='center'>
-				<tr>
-					<td>
-						№
-					</td>
-					<td>
-						{t('Оюутан')}
-					</td>
-					{aggregatedData?.questions?.map((item, key) =>
-						<td
-							key={key}
-							className={`ctooltip ${get_question_title_color(item.question_title)}`}
-						>
-							{key + 1}
-							<span className="ctooltiptext bg-primary text-white fw-bold">{item.question_text}</span>
-						</td>
-					)}
-					<td>
-						{t('Нийт (Зөв)')}
-					</td>
-				</tr>
-			</thead>
-			<tbody align='center'>
-				{data?.map((student, student_key) =>
-					<tr key={student_key}>
+		<div style={{ overflow: 'auto' }}>
+			<table className='studentsQuestionsTable'>
+				<thead align='center'>
+					<tr>
 						<td>
-							{student_key + 1}
+							№
 						</td>
-						<td align='left'>
-							{student.full_name}
+						<td>
+							{t('Оюутан')}
 						</td>
-						{student?.answers.map((answer, answer_key) =>
-							<td key={answer_key}>
-								{answer?.is_answered_right ? <span style={{ color: 'green' }}>O</span> : <span style={{ color: 'red' }}>X</span>}
+						{aggregatedData?.questions?.map((item, key) =>
+							<td
+								key={key}
+								className={`${get_question_title_color(item.question_title)}`}
+								onMouseEnter={(e) => showTooltip(e, item.question_text)}
+								onMouseLeave={hideTooltip}
+							>
+								{key + 1}
 							</td>
 						)}
 						<td>
-							{student?.answers.reduce((acc, answer) => {
-								return answer.is_answered_right ? acc + 1 : acc;
-							}, 0)}
+							{t('Нийт (Зөв)')}
 						</td>
 					</tr>
-				)}
-			</tbody>
-			<tfoot align='center'>
-				<tr>
-					<td colSpan={2}>
-						{t('Нийт (Зөв/Буруу)')}
-					</td>
-					{aggregatedData?.questions_summary && Object.keys(aggregatedData?.questions_summary).map((key, ind) => {
-						const item = aggregatedData?.questions_summary[key]
-						const total_correct = item.correct
-						const total_wrong = item.total - item.correct
-
-						return (
-							<td key={ind}>
-								{total_correct}/{total_wrong}
+				</thead>
+				<tbody align='center'>
+					{data?.map((student, student_key) =>
+						<tr key={student_key}>
+							<td>
+								{student_key + 1}
 							</td>
-						)
-					})}
-					<td>
-					</td>
-				</tr>
-			</tfoot>
-		</table>
+							<td align='left'>
+								{student.full_name}
+							</td>
+							{student?.answers.map((answer, answer_key) =>
+								<td key={answer_key}>
+									{answer?.is_answered_right ? <span style={{ color: 'green' }}>O</span> : <span style={{ color: 'red' }}>X</span>}
+								</td>
+							)}
+							<td>
+								{student?.answers.reduce((acc, answer) => {
+									return answer.is_answered_right ? acc + 1 : acc;
+								}, 0)}
+							</td>
+						</tr>
+					)}
+				</tbody>
+				<tfoot align='center'>
+					<tr>
+						<td colSpan={2}>
+							{t('Нийт (Зөв/Буруу)')}
+						</td>
+						{aggregatedData?.questions_summary && Object.keys(aggregatedData?.questions_summary).map((key, ind) => {
+							const item = aggregatedData?.questions_summary[key]
+							const total_correct = item.correct
+							const total_wrong = item.total - item.correct
+
+							return (
+								<td key={ind}>
+									{total_correct}/{total_wrong}
+								</td>
+							)
+						})}
+						<td>
+						</td>
+					</tr>
+				</tfoot>
+			</table>
+			{visible &&
+				ReactDOM.createPortal(
+					<span
+						className="ctooltiptext bg-primary text-white fw-bold"
+						style={{
+							position: "fixed",
+							...tooltipStyle,
+							zIndex: 9999,
+						}}
+					>
+						{tooltipText}
+					</span>,
+					document.body
+				)}
+		</div>
 	)
 }
