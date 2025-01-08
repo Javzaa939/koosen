73e164024bd99db19be21a8674ddde0b525ff085
duo456 <duo456@gmail.com>
2025-01-08 09:47:30 +0800

    480-comment has been understood and removed

--- backend/apps/scoreregister/views.py
+++ backend/apps/scoreregister/views.py
@@ -1341,18 +1341,6 @@ class TeacherScoreAPIView(
     search_fields = ['student__code', 'student__register_num', 'student__first_name']
 
     def get(self,request):
-        # to join scoreregister with teacherscore in sql level NOTE don't need to join scoreregister just teacherscore
-        # self.queryset = self.queryset.filter(
-        #     Q(
-        #         Q(student__group__profession__department__isnull=False, student__scoreregister__school=F('student__group__profession__department__sub_orgs')) |
-        #         Q(student__group__profession__department__isnull=True, student__scoreregister__school=F('student__group__profession__school'))
-        #     ),
-        #     student__scoreregister__is_delete=False,
-        #     student__scoreregister__lesson_year=F('lesson_year'),
-        #     student__scoreregister__lesson_season=F('lesson_season'),
-        #     student__scoreregister__lesson=F('score_type__lesson_teacher__lesson')
-        # )
-
         lesson_year, lesson_season = get_active_year_season()
         self.queryset = self.queryset.filter(lesson_year=lesson_year, lesson_season=lesson_season)
         school_id = self.request.query_params.get('school')
