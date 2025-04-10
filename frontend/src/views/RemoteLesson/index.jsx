48f5149fc2f0df1aee13e312ed28327493ff86cd
duo456 <duo456@gmail.com>
2025-04-10 13:29:18 +0800

    524-main page items arranging with 4 columns added

--- frontend/src/views/RemoteLesson/index.jsx
+++ frontend/src/views/RemoteLesson/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Plus, Search } from 'react-feather'
-import { Badge, Button, Card, CardBody, CardTitle, Input, PopoverBody, PopoverHeader, UncontrolledPopover } from 'reactstrap'
+import { Badge, Button, Card, CardBody, CardHeader, CardTitle, Col, Input, PopoverBody, PopoverHeader, Row, UncontrolledPopover } from 'reactstrap'
 
 import useApi from '@hooks/useApi';
 import useLoader from '@hooks/useLoader';
@@ -16,7 +16,7 @@ import Addmodal from './components/Add';
 
 function RemoteLesson() {
     // const datas = sample
-    const [ datas, setDatas ] = useState([])
+    const [datas, setDatas] = useState([])
 
     const [addModal, setAddModal] = useState(false)
 
@@ -39,7 +39,7 @@ function RemoteLesson() {
         setAddModal(!addModal)
     }
 
-    const { isLoading, fetchData, Loader } = useLoader({isSmall: true});
+    const { isLoading, fetchData, Loader } = useLoader({ isSmall: true });
 
     const remoteApi = useApi().remote
 
@@ -61,9 +61,9 @@ function RemoteLesson() {
     }, [currentPage]);
 
     // Хуудас солих үед ажиллах хэсэг
-	function handlePagination(page) {
-		setCurrentPage(page.selected + 1);
-	}
+    function handlePagination(page) {
+        setCurrentPage(page.selected + 1);
+    }
 
     const Pagination = getPagination(
         handlePagination,
@@ -81,24 +81,17 @@ function RemoteLesson() {
     }
 
     return (
-        <div>
-            {
-                addModal && (
-                    <Addmodal
-                        open={addModal}
-                        handleModal={toggleAddModal}
-                        refreshDatas={getDatas}
-                    />
-                )
-            }
-            <Card className='p-1 mb-1'>
-                <CardTitle>Зайн сургалт</CardTitle>
-                <CardBody className='d-flex justify-content-between align-items-center'>
-                    <div>
-                        {/* filters */}
-                    </div>
-                    <Button color='primary' onClick={() => toggleAddModal()}><Plus size={14}/> Үүсгэх</Button>
-                </CardBody>
+        <>
+            <Addmodal
+                open={addModal}
+                handleModal={toggleAddModal}
+                refreshDatas={getDatas}
+            />
+            <Card>
+                <CardHeader>
+                    <CardTitle>Зайн сургалт</CardTitle>
+                    <Button color='primary' onClick={() => toggleAddModal()}><Plus size={14} /> Үүсгэх</Button>
+                </CardHeader>
             </Card>
             <div className='d-flex justify-content-between align-items-center mb-1'>
                 <div className='d-flex'>
@@ -122,70 +115,72 @@ function RemoteLesson() {
                         <span className='align-middle ms-50'></span>
                     </Button>
                 </div>
-                <Pagination/>
+                <Pagination />
             </div>
             {
                 isLoading ?
                     <div className='d-flex justify-content-center align-items-center py-2'>
                         {Loader}
                     </div>
-                :
-                    <div className='d-flex flex-wrap gap-1'>
+                    :
+                    <Row>
                         {datas.map((data, idx) => (
-                            <div className='lesson_card d-flex flex-wrap align-items-between align-content-between p-2 shadow-sm' key={idx} style={{ overflow:'hidden' }}>
-                                <div className='w-100'>
-                                    <div className='fw-bold text-center'>
-                                        {data?.title || ''}
+                            <Col md={3} key={idx} className='p-1'>
+                                <div className='lesson_card d-flex flex-wrap align-items-between align-content-between p-2 shadow-sm' style={{ overflow: 'hidden' }}>
+                                    <div className='w-100'>
+                                        <div className='fw-bold text-center'>
+                                            {data?.title || ''}
+                                        </div>
+                                        <div className='d-flex justify-content-between mt-1'>
+                                            <div>
+                                                Багш: <span className='text-decoration-underline' id={`teacher_${idx}`} style={{ cursor: 'help' }}>{data?.teacher_info?.full_name}</span>
+                                                <UncontrolledPopover
+                                                    placement="bottom"
+                                                    trigger="legacy"
+                                                    target={`teacher_${idx}`}
+                                                >
+                                                    <PopoverHeader>
+                                                        Багшийн мэдээлэл
+                                                    </PopoverHeader>
+                                                    <PopoverBody>
+                                                        <div>
+                                                            <div style={{ fontSize: 12 }}>{data?.teacher_info?.sub_org?.name}</div>
+                                                            <b>
+                                                                {data?.teacher_info?.last_name} {data?.teacher_info?.first_name}
+                                                            </b>
+                                                        </div>
+                                                    </PopoverBody>
+                                                </UncontrolledPopover>
+                                            </div>
+                                            <div className='d-flex gap-25'>
+                                                <Badge color='primary' pill title='Оюутны тоо' className='d-flex align-items-center gap-25'>
+                                                    <CiUser style={{ width: "12px", height: "12px" }} /> {data?.students?.length || 0}
+                                                </Badge>
+                                                <Badge color={data?.is_end_exam ? `light-success` : 'light-secondary'} pill title={data?.is_end_exam ? 'Төгсөлтийн шалгалттай' : 'Төгсөлтийн шалгалтгүй'} className='d-flex align-items-center gap-25'>
+                                                    <PiExam style={{ width: "24px", height: "24px" }} />
+                                                </Badge>
+                                                <Badge color={data?.is_certificate ? `light-danger` : 'light-secondary'} pill title={data?.is_certificate ? 'Сертификаттай' : 'Сертификатгүй'} className='d-flex align-items-center gap-25'>
+                                                    <PiCertificate style={{ width: "24px", height: "24px" }} />
+                                                </Badge>
+                                            </div>
+                                        </div>
+                                        <img
+                                            src={data?.image}
+                                            alt='image'
+                                            className='mt-1'
+                                            style={{ height: '200px', width: '100%', objectFit: 'cover', borderRadius: 5 }}
+                                        />
                                     </div>
                                     <div className='d-flex justify-content-between mt-1'>
-                                        <div>
-                                            Багш: <span className='text-decoration-underline' id={`teacher_${idx}`} style={{ cursor: 'help' }}>{data?.teacher_info?.full_name}</span>
-                                            <UncontrolledPopover
-                                                placement="bottom"
-                                                trigger="legacy"
-                                                target={`teacher_${idx}`}
-                                            >
-                                                <PopoverHeader>
-                                                    Багшийн мэдээлэл
-                                                </PopoverHeader>
-                                                <PopoverBody>
-                                                    <div>
-                                                        <div style={{ fontSize: 12 }}>{data?.teacher_info?.sub_org?.name}</div>
-                                                        <b>
-                                                            {data?.teacher_info?.last_name} {data?.teacher_info?.first_name}
-                                                        </b>
-                                                    </div>
-                                                </PopoverBody>
-                                            </UncontrolledPopover>
-                                        </div>
-                                        <div className='d-flex gap-25'>
-                                            <Badge color='primary' pill title='Оюутны тоо' className='d-flex align-items-center gap-25'>
-                                                <CiUser style={{ width: "12px", height: "12px" }}/> {data?.students?.length || 0}
-                                            </Badge>
-                                            <Badge color={data?.is_end_exam ? `light-success` : 'light-secondary'} pill title={data?.is_end_exam ? 'Төгсөлтийн шалгалттай' : 'Төгсөлтийн шалгалтгүй'} className='d-flex align-items-center gap-25'>
-                                                <PiExam style={{ width: "24px", height: "24px" }}/>
-                                            </Badge>
-                                            <Badge color={data?.is_certificate ? `light-danger` : 'light-secondary'} pill title={data?.is_certificate ? 'Сертификаттай' : 'Сертификатгүй'} className='d-flex align-items-center gap-25'>
-                                                <PiCertificate style={{ width: "24px", height: "24px" }}/>
-                                            </Badge>
-                                        </div>
+                                        <div></div>
+                                        <a href={`/remote_lesson/${data?.id}`} className='d-flex align-items-center gap-25 pe-50 fw-bold text-decoration-underline'>Дэлгэрэнгүй <FaAngleDoubleRight /></a>
                                     </div>
-                                    <img
-                                        src={data?.image}
-                                        alt='image'
-                                        className='mt-1'
-                                        style={{ height: '200px', width: '100%', objectFit: 'cover', borderRadius: 5 }}
-                                    />
                                 </div>
-                                <div className='d-flex justify-content-between mt-1'>
-                                    <div></div>
-                                    <a href={`/remote_lesson/${data?.id}`} className='d-flex align-items-center gap-25 pe-50 fw-bold text-decoration-underline'>Дэлгэрэнгүй <FaAngleDoubleRight/></a>
-                                </div>
-                            </div>
+                            </Col>
                         ))}
-                    </div>
+                    </Row>
             }
-        </div>
+        </>
     )
 }
 
