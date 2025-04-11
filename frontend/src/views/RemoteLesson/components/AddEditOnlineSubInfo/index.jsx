a294b012ff1ec08e8c933d51ddf8bb93e053f65d
duo456 <duo456@gmail.com>
2025-04-11 13:20:48 +0800

    527-text type adding added

--- frontend/src/views/RemoteLesson/components/AddEditOnlineSubInfo/index.jsx
+++ frontend/src/views/RemoteLesson/components/AddEditOnlineSubInfo/index.jsx
@@ -1,7 +1,15 @@
 import { t } from 'i18next';
-import { Fragment, useCallback, useEffect, useState } from 'react';
+import { Fragment, useCallback, useEffect, useRef, useState } from 'react';
 import { X } from 'react-feather';
 import { useForm, Controller } from "react-hook-form";
+import '@styles/react/libs/flatpickr/flatpickr.scss';
+import classNames from 'classnames';
+import { ReactSelectStyles } from '@src/utility/Utils';
+import Select from 'react-select'
+
+// editor imports
+import ReactQuill from 'react-quill';
+import 'quill/dist/quill.snow.css'
 
 import {
     Button,
@@ -16,15 +24,25 @@ import {
 
 import useApi from '@hooks/useApi';
 import useLoader from '@hooks/useLoader';
-import '@styles/react/libs/flatpickr/flatpickr.scss';
 import { convertDefaultValue } from "@utils";
-
 import empty from "@src/assets/images/empty-image.jpg";
 import { onChangeFile } from '@src/views/RemoteLesson/utils';
+
 import ScrollSelectFilter from '../ScrollSelectFilter';
 
+const fileTypeOptions = [
+    { id: 1, name: "PDF" },
+    { id: 2, name: "Video хичээл" },
+    { id: 3, name: "TEXT" },
+    // { id: 5, name: "Шалгалт" },
+]
+
+const inputNameFileType = 'file_type'
+const inputNameFileTypeText = 'text'
+
 const AddEditOnlineSubInfo = ({ open, handleModal, refreshDatas, editData, elearnId, onlineInfoId }) => {
-    const { control, handleSubmit, setError, setValue, reset, formState: { errors } } = useForm()
+    const { control, handleSubmit, setError, setValue, reset, formState: { errors }, watch } = useForm()
+    const formValues = watch()
     const { fetchData, isLoading, Loader } = useLoader({ isFullScreen: true });
     const remoteApi = useApi().remote
 
@@ -61,6 +79,35 @@ const AddEditOnlineSubInfo = ({ open, handleModal, refreshDatas, editData, elear
         }
     }
 
+    // #region editor code
+    const quillRef = useRef(null);
+
+    const modules = {
+        toolbar: [
+            [{ 'font': [] }, { 'size': ['small', 'medium', 'large', 'huge'] }],
+            [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
+            [{ 'align': [] }],
+            ['bold', 'italic', 'underline', 'strike'],
+            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+            [{ 'indent': '-1' }, { 'indent': '+1' }],
+            ['link'],
+            [{ color: [] }, { background: [] }],
+            ['blockquote'],
+            //   ['image'],
+            ['clean']
+        ],
+    };
+
+    const formats = [
+        'header', 'bold', 'italic', 'underline', 'strike',
+        'align', 'list', 'indent',
+        'size', 'link',
+        // 'image',
+        'color', 'background',
+        'clean'
+    ];
+    // #endregion
+
     return (
         <Fragment>
             <Modal
@@ -86,32 +133,95 @@ const AddEditOnlineSubInfo = ({ open, handleModal, refreshDatas, editData, elear
                 <ModalBody className="flex-grow-50 mb-3 t-0">
                     {isLoading && Loader}
                     <Row tag={Form} onSubmit={handleSubmit(onSubmit)}>
-                        <Col md={12}>
-                            <Row>
-                                <Col md={12}>
-                                    <Label className='form-label' for='title'>
-                                        {t('Хичээлийн нэр гарчиг')}
-                                    </Label>
-                                    <Controller
-                                        defaultValue=''
-                                        control={control}
-                                        name='title'
-                                        render={({ field }) => (
-                                            <Input
-                                                {...field}
-                                                type='text'
-                                                name={field.name}
-                                                id={field.name}
-                                                bsSize='sm'
-                                                placeholder={t('Хичээлийн нэр гарчиг')}
-                                                invalid={errors[field.name] && true}
-                                            />
-                                        )}
+                        <Col md={6}>
+                            <Label className='form-label' for='title'>
+                                {t('Хичээлийн нэр гарчиг')}
+                            </Label>
+                            <Controller
+                                defaultValue=''
+                                control={control}
+                                name='title'
+                                render={({ field }) => (
+                                    <Input
+                                        {...field}
+                                        type='text'
+                                        name={field.name}
+                                        id={field.name}
+                                        bsSize='sm'
+                                        placeholder={t('Хичээлийн нэр гарчиг')}
+                                        invalid={errors[field.name] && true}
                                     />
-                                    {errors.title && <FormFeedback className='d-block'>{t(errors.title.message)}</FormFeedback>}
-                                </Col>
-                            </Row>
+                                )}
+                            />
+                            {errors.title && <FormFeedback className='d-block'>{t(errors.title.message)}</FormFeedback>}
                         </Col>
+                        <Col md={6}>
+                            <Label className="form-label" for={inputNameFileType}>
+                                {t('Материалын төрөл')}
+                            </Label>
+                            <Controller
+                                control={control}
+                                defaultValue=''
+                                name={inputNameFileType}
+                                render={({ field }) => {
+                                    return (
+                                        <Select
+                                            name={field.name}
+                                            id={field.name}
+                                            classNamePrefix='select'
+                                            isClearable
+                                            className={classNames('react-select', { 'is-invalid': errors[field.name] })}
+                                            isLoading={isLoading}
+                                            placeholder={t('-- Сонгоно уу --')}
+                                            options={fileTypeOptions || []}
+                                            value={field.value && fileTypeOptions.find((c) => c.id === field.value)}
+                                            noOptionsMessage={() => t('Хоосон байна.')}
+                                            onChange={(val) => {
+                                                field.onChange(val?.id || '')
+                                            }}
+                                            styles={ReactSelectStyles}
+                                            getOptionValue={(option) => option.id}
+                                            getOptionLabel={(option) => option.name}
+                                        />
+                                    )
+                                }}
+                            ></Controller>
+                            {errors[inputNameFileType] && <FormFeedback className='d-block'>{t(errors[inputNameFileType].message)}</FormFeedback>}
+                        </Col>
+                        {formValues[inputNameFileType] === 3 && <Col md={12} className='mt-1'>
+                            <Label className="form-label" for={inputNameFileTypeText}>
+                                {t('ТEXT төрлийн мэдээлэл')}
+                            </Label>
+                            <Controller
+                                control={control}
+                                defaultValue=''
+                                name={inputNameFileTypeText}
+                                render={({ field }) => {
+                                    return (
+                                        <>
+                                            <style>
+                                                {`
+                                                .custom-quill .ql-container {
+                                                    min-height: 100px;
+                                                }
+                                            `}
+                                            </style>
+                                            <ReactQuill
+                                                {...field}
+                                                id={field.name}
+                                                ref={quillRef}
+                                                placeholder={t('ТEXT төрлийн мэдээлэл')}
+                                                modules={modules}
+                                                formats={formats}
+                                                theme="snow"
+                                                className='custom-quill'
+                                            />
+                                        </>
+                                    )
+                                }}
+                            ></Controller>
+                            {errors[inputNameFileTypeText] && <FormFeedback className='d-block'>{t(errors[inputNameFileTypeText].message)}</FormFeedback>}
+                        </Col>}
                         <Col md={12} className="text-center mt-2">
                             <Button className='me-2' color="primary" type="submit">
                                 {t('Хадгалах')}
