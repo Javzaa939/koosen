3371181233ca4ba6a4db4955f46bc293f318cfc1
duo456 <duo456@gmail.com>
2025-05-16 15:33:06 +0800

    544-closed issue-mistake change cancelled

--- frontend/src/layouts/VerticalLayout.js
+++ frontend/src/layouts/VerticalLayout.js
@@ -100,7 +100,7 @@ const VerticalLayout = (props) =>
                         childrenDatas = checkPerm(childrenDatas, menus, 'lms-elselt-justice-read', 'elselt3_3')
                         childrenDatas = checkPerm(childrenDatas, menus, 'lms-elselt-army-read', 'elselt6_1')
                         /** Элсэлтийн шалгалтын асуулт */
-                        childrenDatas = checkPerm(childrenDatas, menus, 'lms-elselt-army-read', 'elselt6_2')
+                        childrenDatas = checkPerm(childrenDatas, menus, 'lms-elselt-question-read', 'elselt6_2')
 
                         childrenDatas = checkPerm(childrenDatas, menus, 'lms-elselt-approve-read', 'elselt7')
                         childrenDatas = checkPerm(childrenDatas, menus, 'lms-elselt-log-read', 'elselt7_1')
