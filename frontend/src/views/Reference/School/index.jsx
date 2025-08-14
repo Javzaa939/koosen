71bd3eacbbc506dec71359291855edce52763b84
duo456 <duo456@gmail.com>
2025-08-15 02:42:38 +0800

    ref.school-clean

--- frontend/src/views/Reference/School/index.jsx
+++ frontend/src/views/Reference/School/index.jsx
@@ -1,7 +1,7 @@
 // ** React Imports
 import { Fragment, useState, useEffect, useContext} from 'react'
 
-import { Col, Card, Input, CardTitle, CardHeader, Spinner, Button, Label } from 'reactstrap'
+import { Card, CardTitle, CardHeader, Spinner, Button } from 'reactstrap'
 
 import { ChevronDown, Plus } from 'react-feather'
 import DataTable from 'react-data-table-component'
@@ -9,9 +9,7 @@ import DataTable from 'react-data-table-component'
 import useApi from '@hooks/useApi';
 import useLoader from '@hooks/useLoader';
 
-import { getPagination } from '@utils';
 import AuthContext from '@context/AuthContext'
-import SchoolContext from '@context/SchoolContext'
 
 import { getColumns } from './helpers';
 import UpdateModal from "./Edit"
