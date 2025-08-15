2751c4c7827f8b77618dad237142a1553f65cd74
anandaa12 <anand.e145@gmail.com>
2025-08-15 16:23:16 +0800

    update

--- frontend/src/navigation/vertical/OnlineLesson.js
+++ frontend/src/navigation/vertical/OnlineLesson.js
@@ -1,27 +1,25 @@
-import { Circle } from "react-feather";
-import React from "react";
-import { MdPlayLesson } from "react-icons/md";
+import { MonitorPlayIcon, Circle } from 'lucide-react';
 const OnlineLessonRoutes = [
-  {
-    id: "onlinelesson",
-    title: "Онлайн хичээл",
-    icon: <MdPlayLesson />,
-    navLink: "/online_lessons",
-    navChildren:[
-      {
-        id: "onlinelesson1",
-        title: "Хичээлүүд",
-        icon: <Circle size={8} />,
-        navLink: "online_lesson",
-      },
-      {
-        id:'onlinelesson2',
-        title:'Хичээлийн материал',
-        icon: <Circle size={8} />,
-        navLink:"material"
-      },
-    ]
-  },
+    {
+        id: 'onlinelesson',
+        title: 'Онлайн хичээл',
+        icon: <MonitorPlayIcon />,
+        navLink: '/online_lessons',
+        navChildren: [
+            {
+                id: 'onlinelesson1',
+                title: 'Хичээлүүд',
+                icon: <Circle size={8} />,
+                navLink: 'online_lesson',
+            },
+            {
+                id: 'onlinelesson2',
+                title: 'Хичээлийн материал',
+                icon: <Circle size={8} />,
+                navLink: 'material',
+            },
+        ],
+    },
 ];
 
 export default OnlineLessonRoutes;
