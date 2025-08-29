8d6f480c4d5607a80134a81d650218dead775772
duo456 <duo456@gmail.com>
2025-08-29 16:19:40 +0800

    clean accidental wrong commit

--- backend/main/settings/base.py
+++ backend/main/settings/base.py
@@ -90,7 +90,6 @@ INSTALLED_APPS = [
     "esis",
     "django_user_agents",
     "django_crontab",
-    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -98,7 +97,6 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -112,9 +110,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "main.urls"
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
 
 TEMPLATES = [
     {
